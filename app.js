71501272aa4eafafd6f191bab46356dce4adbac2
Anja <anja.labak12345@gmail.com>
2023-04-27 21:12:11 +0200

    Adding environment variables, APIs, and Asynchronous function

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const path = require('path');
+const fs = require('fs');
+
 
 // Google OAuth2Client
 
@@ -7,7 +9,7 @@ const { google } = require('googleapis');
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground/';
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
 
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 
@@ -23,3 +25,55 @@ const drive = google.drive({
     version: 'v3',
     auth: oauth2Client
 })
+
+// API
+
+// npm install screenshotmachine --save
+
+var screenshotmachine = require('screenshotmachine');
+
+var customerKey = process.env.API_KEY;
+    secretPhrase = ''; //leave secret phrase empty, if not needed
+    options = {
+      //mandatory parameter
+      url : 'https://planethome-invest.com/en/',
+      // all next parameters are optional, see our website screenshot API guide for more details
+      dimension : '1920x1080', // or "1366xfull" for full length screenshot
+      format: 'jpg',
+      cacheLimit: '0',
+      delay: '200',
+      zoom: '100'
+    }
+
+var apiUrl = screenshotmachine.generateScreenshotApiUrl(customerKey, secretPhrase, options);
+
+// Screenshot + await function
+const { dirname } = require('path');
+var output = 'ID_name.jpg';
+screenshotmachine.readScreenshot(apiUrl).pipe(fs.createWriteStream(output).on('close', async function() {
+    console.log('Screenshot saved as ' + output);
+    await uploadFile();
+  }));
+  
+//Function that uploads file to Google Drive
+
+const filePath = path.join(__dirname, 'ID_name.jpg')
+
+ async function uploadFile(){
+    try {
+        const response = await drive.files.create({
+            requestBody: {
+                name: 'ID_name.jpg',
+                mimeType: 'image/jpeg'
+            },
+            media: {
+                mimeType: 'image/jpeg',
+                body: fs.createReadStream(filePath)
+            }
+        })
+
+        console.log(response.data);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
